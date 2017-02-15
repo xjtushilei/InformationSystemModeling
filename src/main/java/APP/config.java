ad6b27f86ac5446364d5b1a6b295b2a15a5c3ab1
xjtushilei <xjtushilei@foxmail.com>
2017-02-15 11:04:01 +0800

    小bug修改

--- src/main/java/APP/config.java
+++ src/main/java/APP/config.java
@@ -8,7 +8,7 @@ package APP;
 public class config {
 	/**
 	 * 芒果DB的配置
-	 */
+	 */ 
 	public static String MongoDB_IP="202.117.54.85";
 	public static int MongoDB_Port=27017;
 	public static String MongoDB_DataBase="SpiderNews";
